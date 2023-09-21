4e3d61485e846e6f5ab84ad0d856590c9ae0fe64
konain1 <kaunainkasab@gmail.com>
2023-09-21 11:24:16 +0530

    memo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useMemo } from 'react';
 
 function App() {
+
+  const [add, setAdd] = useState(0);
+  const [min, setMin] = useState(99);
+
+  const incrementAdd = () => {
+    setAdd(add + 1);
+  };
+
+  const multi = useMemo(()=>
+  {
+    console.log('mutli runs')
+    return add*10;
+  },[add])
+
+  const decrementMin = () => {
+    setMin(min - 1);
+  };
+ 
+
+  
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>{multi}</p>
+        <p>{add}</p>
+        <button onClick={incrementAdd}>Add</button>
+        <p>{min}</p>
+        <button onClick={decrementMin}>Min</button>
       </header>
     </div>
   );
